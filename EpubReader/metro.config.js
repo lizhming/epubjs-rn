b41f792bb6a6030b6a20488de20fae959c580c6c
lizhming <flymaxelib@hotmail.com>
2020-04-08 03:20:35 +0900

    package version upgrade

--- EpubReader/metro.config.js
+++ EpubReader/metro.config.js
@@ -5,7 +5,20 @@
  * @format
  */
 
+const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
+  resolver: {
+    blacklistRE: blacklist([
+      /node_modules\/.*\/node_modules\/react-native\/.*/,
+
+      // /node_modules\/rn-fetch-blob\/node_modules\/react-native\/.*/,
+      // /node_modules\/epubjs-rn\/node_modules\/react-native\/.*/,
+      // /node_modules\/epubjs-rn\/node_modules\/.*\/node_modules\/react-native\/.*/,
+      // /node_modules\/react-native-zip-archive\/node_modules\/react-native\/.*/,
+      // /node_modules\/@lightbase\/react-native-orientation\/node_modules\/react-native\/.*/,
+    ]),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
